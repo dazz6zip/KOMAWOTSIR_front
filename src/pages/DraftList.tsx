cc7b5be08a95205f1c49f83ba16494ac674e5d36
dazz6 <dazz6@kakao.com>
2024-11-28 17:58:15 +0900

    merge;

--- src/pages/DraftList.tsx
+++ src/pages/DraftList.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import ButtonL from "../components/common/ButtonL";
 import Title from "../components/common/Title";
+
 import { useQuery } from "react-query";
 import { DraftLoad, IDraftLoad } from "../fetcher";
 import { useState } from "react";
