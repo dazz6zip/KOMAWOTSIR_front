968c12e1b34da93eb83c0dbe9e035192e0bdbcff
hyo <hyoseonee0729@naver.com>
2024-12-16 15:11:34 +0900

    광고 게재

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
-import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +14,13 @@ fontLink.href =
   "https://fonts.googleapis.com/css2?family=Jua&family=Nanum+Pen+Script&display=swap";
 fontLink.rel = "stylesheet";
 document.head.appendChild(fontLink);
+
+// 광고 스크립트 추가
+const adScript = document.createElement("script");
+adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1639750615440525"; // 광고 스크립트 URL
+adScript.async = true;
+adScript.crossOrigin = "anonymous";
+document.head.appendChild(adScript);
 
 ReactDOM.render(
   <React.StrictMode>
